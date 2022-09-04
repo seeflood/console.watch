a45fc87a1c23e7b33c7c58775def54d0a285b1e3
seeflood <zhou.qunli@foxmail.com>
2022-09-04 22:38:06 +0800

    activate or open
    Signed-off-by: seeflood <zhou.qunli@foxmail.com>

--- watch/js/popup.js
+++ watch/js/popup.js
@@ -5,12 +5,20 @@
 
 chrome.commands.onCommand.addListener(function (command) {
   console.log("Command:", command);
+  let url = null;
+  // split `command@url`
+  if (command.indexOf("@") > -1) {
+    const idx = command.indexOf("@");
+    url = command.substring(1 + idx, command.length);
+    command = command.substring(0, idx);
+  }
   chrome.tabs.query(
     {
       // currentWindow: true,
     },
     function (tabs) {
       // console.log(tabs);
+      // find the target tab
       for (const tab of tabs) {
         if (tab.title.indexOf(command) > -1) {
           console.log(tab.title);
@@ -23,6 +31,8 @@ chrome.commands.onCommand.addListener(function (command) {
           return;
         }
       }
+      // open a new tab if not exist
+      window.open(url);
     }
   );
 });
